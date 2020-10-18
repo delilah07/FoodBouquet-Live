4a54d9016edaee38d7796a5964fb76f91c3ce094
Abrosimova Iryna <47872020+delilah07@users.noreply.github.com>
2020-10-19 00:03:33 +0200

    made 3 more sections

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 $(document).ready(function(){
-    // search input
-    $('.search__btn').on('click', function(){
+
+    // Header Scrolling
+    $(window).scroll(function(){
+        heightIntro = $('#intro').height() - 18;
+        if ($(window).scrollTop() > heightIntro) {
+            $('.header').addClass('scrolling');
+        } else {
+            $('.header').removeClass('scrolling');
+        }
+        console.log(heightIntro);
+        console.log($(window).scrollTop());
+    });
+
+    // Search Input
+    $('.search__btn').click(function(){
         $('.search__input').toggleClass('active');
     });
 
@@ -12,6 +25,9 @@ $(document).ready(function(){
         arrows: false,
         dots: false,
         asNavFor: '.intro__dots',
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
     });
     $('.intro__dots').slick({
         slidesToShow: 3,
@@ -20,6 +36,52 @@ $(document).ready(function(){
         dots: false,
         arrows: false,
         centerMode: true,
-        focusOnSelect: true
+        focusOnSelect: true,
     });
+
+    // Menu Btn
+    $('.menu__btn').click(function(){
+        $('.menu__btn').toggleClass('active');
+        $('.header__nav').toggleClass('active');
+    })
+    $('.nav__list-item').click(function(){
+        $('.menu__btn').removeClass('active');
+        $('.header__nav').removeClass('active');
+    });
+
+    // Scroll Reveal
+    window.sr = ScrollReveal();
+
+    sr.reveal('.animate-left',{
+        origin:'left',
+        duration: 1000,
+        distance:'25rem',
+        delay:500
+    });
+    sr.reveal('.animate-right',{
+        origin:'right',
+        duration: 1000,
+        distance:'25rem',
+        delay:600
+    });
+    sr.reveal('.animate-top',{
+        origin:'top',
+        duration: 1000,
+        distance:'25rem',
+        delay:300
+    });
+    sr.reveal('.animate-bottom',{
+        origin:'bottom',
+        duration: 1000,
+        distance:'25rem',
+        delay:800
+    });
+    sr.reveal('.spotlight', {
+        distance: '0px',
+        opacity: 0,
+        duration: 2000,
+        delay:600
+    });
+
 });
+
